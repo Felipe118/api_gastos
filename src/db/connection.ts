b2532ae0870abfaa0ba15c1c626f84205033f08f
Felipe118 <lipebezerraa@gmail.com>
2022-05-13 22:09:53 -0300

    alterações

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,13 +1,45 @@
-const {Sequelize} = require('sequelize')
+import { Sequelize } from "sequelize"
 
-const sequelize = new Sequelize('api_gastos','root','root', {
-    host:'localhost',
-    dialect: 'mysql'
-})
+async function connect() {
+        
+     const sequelize = new Sequelize('api_gastos','root','root', {
+        host:'localhost',
+        dialect: 'mysql'
+    })
 
-try {
-    sequelize.authenticate()
-    console.log('Conectado ao Sequelize')
-} catch (error) {
-    console.log('ERROR:', error)
+    try {
+        await sequelize.authenticate()
+        console.log('Conectado ao Sequelize')
+    } catch (error) {
+        console.log('ERROR:', error)
+    }
 }
+
+/**import { Sequelize, Options } from 'sequelize';
+const dbUrl: string = process.env.DB_URL || '';
+const nodeEnv: string = process.env.NODE_ENV || '';
+
+if (!dbUrl) {
+  console.log('Please create .env file, refer .env.sample');
+  process.exit(0);
+}
+
+let optionsObj: object = { benchmark: true, logging: console.log };
+
+if (nodeEnv && nodeEnv === 'production') {
+  optionsObj = { logging: false };
+}
+const options: Options = optionsObj;
+
+export const sequelize: Sequelize = new Sequelize(dbUrl, options);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully..');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  }); */
+
+export default connect;
